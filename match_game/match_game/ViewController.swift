1020beca61c64dac08e431259b627813dfdbaf10
Hai Phan Thanh <haiphan5289@gmail.com>
2019-07-01 15:02:20 +0700

    full code & comment

---
+++ match_game/match_game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  match_game
+//
+//  Created by HaiPhan on 6/30/19.
+//  Copyright © 2019 HaiPhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
